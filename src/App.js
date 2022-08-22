23dca57f1d8cf8e106c4c0bc389b556a816d51c4
Clifton Carvalho <clifton.carvalho@thoughtworks.com>
2022-08-22 12:04:11 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Awesome Iframes</h3>
+      <iframe src="http://127.0.0.1:8081/index.html" width={400} height={200}></iframe>
     </div>
   );
 }
